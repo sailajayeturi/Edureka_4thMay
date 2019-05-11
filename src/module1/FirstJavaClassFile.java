9389921c1d41ab1b54dc0f13f5256284a1f223b7
Meghaha <meghanayeturi@gmail.com>
2019-05-11 13:09:25 -0400

    first checkin

---
+++ src/module1/FirstJavaClassFile.java
@@ -0,0 +1,14 @@
+package module1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstJavaClassFile {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver= new ChromeDriver();
+driver.get("https://facebook.com");
+	}
+
+}
