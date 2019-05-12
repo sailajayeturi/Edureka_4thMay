65c2039ae73c3e1ac66a2200ff2fa68156a07c0b
Meghaha <meghanayeturi@gmail.com>
2019-05-12 19:27:27 -0400

    New Change

--- src/module1/FirstJavaClassFile.java
+++ src/module1/FirstJavaClassFile.java
@@ -1,14 +1,28 @@
 package module1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstJavaClassFile {
 
 	public static void main(String[] args) {
-		
+		//open chrome browser
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\megha\\Downloads\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
-driver.get("https://facebook.com");
+		
+		//open AUT(under test application)
+		driver.get("https://www.facebook.com");
+		
+		//ID
+		driver.findElement(By.id("u_0_c")).sendKeys("sailaja");
+		//Name
+		driver.findElement(By.name("lastname")).sendKeys("yeturi");
+		//ClassName
+		driver.findElement(By.className("inputtext")).sendKeys("yeturi");
+		//initializing the browser
+		
+
 	}
 
 }
